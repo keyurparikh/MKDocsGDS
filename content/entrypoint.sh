494e5e4044d911659bd20a5f22cab034946ef4da
Keyur Parikh <keyur.parikh@yahoo.com>
2021-11-09 14:09:18 +0000

    ss

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -4,7 +4,4 @@ cf_opts=
 if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
   cf_opts="--skip-ssl-validation"
 fi
-cf api ${INPUT_API} ${cf_opts}
-CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
-cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-cf push -f ${INPUT_MANIFEST}
+echo "hi"
